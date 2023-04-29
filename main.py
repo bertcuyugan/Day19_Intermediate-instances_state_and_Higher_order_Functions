8f1f934f943f490b29f55ff1623080a8845b4ca1
Herbert Cuyugan <herbert.cuyugan@pentanasolutions.com>
2023-04-29 14:21:02 +1000

    adjusted counter clockwise and clockwise movement

--- main.py
+++ main.py
@@ -9,13 +9,24 @@ def move_forward():
 #S-move backward
 def move_backward():
     tim.backward(10)
+
+
 #A-counter-clockwise
-def move_counter_clockwise():
-    tim.circle(50)
+# def move_counter_clockwise():
+    # tim.circle(50)
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
 
 #D-clockwise
-def move_clockwise():
-    tim.circle(-50)
+#def move_clockwise():
+    # tim.circle(-50)
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
 #C-clear-drawings
 def clear():
     tim.clear()
@@ -28,8 +39,10 @@ screen.listen()
 
 screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="s", fun=move_backward)
-screen.onkey(key="a", fun=move_counter_clockwise)
-screen.onkey(key="d", fun=move_clockwise)
+# screen.onkey(key="a", fun=move_counter_clockwise)
+# screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
 screen.onkey(key="c", fun=clear)
 
 screen.exitonclick()
